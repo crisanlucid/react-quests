255a9c6d8760f54e08692dfe264e03bcf8629e76
crisanlucid <crisan.lucid@yahoo.com>
2019-04-17 18:14:52 +0200

    Updates

---
+++ precache-manifest.836ccd9e023ae0b811f8304d6a4033d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-quests/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "88e247b46108570081c2",
+    "url": "/react-quests/static/js/runtime~main.fc1a7af9.js"
+  },
+  {
+    "revision": "ef0041333e72b0e29cc2",
+    "url": "/react-quests/static/js/main.92f56737.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/react-quests/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "ef0041333e72b0e29cc2",
+    "url": "/react-quests/static/css/main.ec4a0308.chunk.css"
+  },
+  {
+    "revision": "6051a8699b292fb3be5e6c85ed5c2c93",
+    "url": "/react-quests/index.html"
+  }
+];
